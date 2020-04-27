ddda45a189b0e8c97cb6bf18ecd1439f2b235123
Michiel Dral <m.c.dral@gmail.com>
2020-04-27 17:15:48 +0200

    Hmmm

--- spigot-example/plugins/dral/src/warp.js
+++ spigot-example/plugins/dral/src/warp.js
@@ -1,29 +1,37 @@
-let fs = require('fs')
+let fs = require("fs");
 
-let Location = Java.type('org.bukkit.Location');
-let ChatColor = Java.type('org.bukkit.ChatColor');
+let Location = Java.type("org.bukkit.Location");
+let ChatColor = Java.type("org.bukkit.ChatColor");
 
 let command_error = (command, error) => {
-  return `${ChatColor.BOLD}${ChatColor.RED}${command}: ${ChatColor.RESET}${ChatColor.RED}${error}`;
-}
+  return `${ChatColor.DARK_RED}${command}: ${ChatColor.RESET}${ChatColor.RED}${error}`;
+};
 let command_success = (command, message) => {
-  return `${ChatColor.BOLD}${ChatColor.GREEN}${command}: ${ChatColor.RESET}${ChatColor.GREEN}${message}`;
-}
+  return `${ChatColor.DARK_GREEN}${command}: ${ChatColor.RESET}${ChatColor.GREEN}${message}`;
+};
 
-module.exports = (plugin) => {
+module.exports = plugin => {
   let warps = {};
 
   try {
-    warps = JSON.stringify(fs.readFileSync('./warps.json').toString());
+    warps = JSON.parse(fs.readFileSync("./warps.json").toString());
   } catch (error) {
     console.log(`Loading wraps error:`, error);
   }
 
-  plugin.command('setwarp', {
+  plugin.command("setwarp", {
     onCommand: (sender, command, alias, [warp_name]) => {
       if (warps[warp_name]) {
-        if (warps[warp_name].player.toLowerCase() !== sender.getName().toLowerCase()) {
-          sender.sendMessage(command_error('/setwarp', `Warp already owned by ${warps[warp_name].player}`))
+        if (
+          warps[warp_name].player.toLowerCase() !==
+          sender.getName().toLowerCase()
+        ) {
+          sender.sendMessage(
+            command_error(
+              "/setwarp",
+              `Warp already owned by ${warps[warp_name].player}`
+            )
+          );
           return;
         }
       }
@@ -34,34 +42,43 @@ module.exports = (plugin) => {
         y: sender.getLocation().getY(),
         z: sender.getLocation().getZ(),
         yaw: sender.getLocation().getYaw(),
-        pitch: sender.getLocation().getPitch(),
-      }
+        pitch: sender.getLocation().getPitch()
+      };
 
-      fs.writeFileSync('./warps.json', JSON.stringify(warps));
+      fs.writeFileSync("./warps.json", JSON.stringify(warps));
 
-      sender.sendMessage(command_success('/setwarp', `Warp "${warp_name}" set!`))
+      plugin.java.getServer().broadcastMessage​(
+        command_success("/setwarp", `${sender.getName()} created warp "${warp_name}"!`)
+      );
     },
     onTabComplete: (sender, command, alias, args) => {
       // let result = onTabComplete(plugin, sender, args);
       return [];
-    },
+    }
   });
 
-  plugin.command('warp', {
+  plugin.command("warp", {
     onCommand: (sender, command, alias, [warp_name]) => {
       let warp = warps[warp_name];
       if (!warp) {
-        sender.sendMessage(command_error('/warp', `No warp found :(`));
-        return
+        sender.sendMessage(command_error("/warp", `No warp found :(`));
+        return;
       }
 
-      let location = new Location(sender.getWorld(), warp.x, warp.y, warp.z, warp.yaw, warp.pitch)
-      sender.teleport(location)
-      sender.sendMessage(command_success('/warp', `Warped to "${warp_name}"!`))
+      let location = new Location(
+        sender.getWorld(),
+        warp.x,
+        warp.y,
+        warp.z,
+        warp.yaw,
+        warp.pitch
+      );
+      sender.teleport(location);
+      plugin.java.getServer().broadcastMessage​(command_success("/warp", `${sender.getDisplayName()} warped to "${warp_name}"!`));
     },
     onTabComplete: (sender, command, alias, args) => {
       // let result = onTabComplete(plugin, sender, args);
       return Object.keys(warps);
-    },
+    }
   });
-}
+};
