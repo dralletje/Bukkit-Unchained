1fc07a82579f56484a4e77b49a2330ae0a4f8109
Michiel Dral <m.c.dral@gmail.com>
2020-04-27 22:46:46 +0200

    Hey

--- spigot-example/plugins/dral/src/take.js
+++ spigot-example/plugins/dral/src/take.js
@@ -2,12 +2,15 @@ let ItemStack = Java.type('org.bukkit.ItemStack');
 
 module.exports = (plugin) => {
   plugin.command("take", {
-    onCommand: (sender, command, alias, [warp_name]) => {
+    onCommand: (sender, command, alias, args) => {
       let block = sender.getTargetBlock(20);
+      console.log(`block:`, block)
       let material = block.getType();
+      console.log(`material:`, material)
       let itemstack = new ItemStack(material);
+      console.log(`itemstack:`, itemstack)
 
-      sender.setItemInMainHand(itemstack)
+      sender.getInventory().setItemInMainHand(itemstack)
     },
   });
 }
